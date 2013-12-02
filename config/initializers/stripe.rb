edf351cc9b095e8c971d9e53a2153b0604c57b4e
K C <aries0420@gmail.com>
2013-12-02 02:27:39 -0800

    Added more stripe events to handle users' activities

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,4 +6,15 @@ StripeEvent.setup do
     user = User.find_by_customer_id(event.data.object.customer)
     user.expire
   end
+
+  subscribe 'charge.succeeded' do |event|
+    user = User.where(customer_token: event.data.object.customer).first
+    Payment.create(user_id: user.id, amount: event.data.object.amount, reference_id: event.data.object.id)
+  end
+
+  subscribe 'charge.failed' do |event|
+    user = User.where(customer_token: event.data.object.customer).first
+    user.deactivate!
+    AppMailer.delay.payment_failure(user)
+  end
 end
