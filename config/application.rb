5d4a19c796f283a87c27bafd1842384e9033617b
K C <aries0420@gmail.com>
2013-12-01 22:37:52 -0800

    Assets compiled for Heroku

--- config/application.rb
+++ config/application.rb
@@ -84,5 +84,8 @@ module Codeable
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
   end
 end
