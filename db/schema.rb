a15f0aa8bbfa1403c44a9ac7b528f37df2f6a720
K C <aries0420@gmail.com>
2013-12-02 12:25:59 -0800

    Adding assignment resource to repo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017180539) do
+ActiveRecord::Schema.define(:version => 20131202193435) do
+
+  create_table "assignments", :force => true do |t|
+    t.string   "category"
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
